eefd27f6d967ad8f9ec8333b0a4e8c952e352dfb
Juan Andres Alfonso Gimenez <juanx.alfonso@gmail.com>
2022-05-31 21:08:12 -0400

    agregado el swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+  .setTitle('API EDGE')
+  .setDescription('API de productos')
+  .setVersion('1.0')
+  .addTag('products')
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('doc', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
